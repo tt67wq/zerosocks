782cfb1d0b2424341dec449f914218d9e2f7b9a7
tt67wq <alex@mys4s.cn>
2020-01-17 15:21:48 +0800

    白雪公主诠释了，七个小屌丝对她再好，也不及高富帅的一个吻。

--- apps/client/lib/client/receiver.ex
+++ apps/client/lib/client/receiver.ex
@@ -10,17 +10,18 @@ defmodule Client.Receiver do
 
   def run() do
     {sid, data} = Client.Tunnel.decode_recv()
-
     Logger.debug("recv from tunnel: #{inspect(data)}")
+    process_data(sid, data)
+    run()
+  end
 
+  defp process_data(sid, data) do
     case Client.SockStore.lookup(sid) do
       nil ->
-        Logger.warn("ignored msg: #{inspect(data)}")
+        Logger.warn("ignored data: #{inspect(data)}")
 
       client ->
         Socket.Stream.send(client, data)
     end
-
-    run()
   end
 end
