25cefb7c7172f5e92ac6f5f7bd2003559a8b863e
tt67wq <alex@mys4s.cn>
2020-01-17 09:03:18 +0800

    减肥这件事情吧，减的快反弹的快，减的慢放弃的快。

--- apps/common/lib/compressor.ex
+++ apps/common/lib/compressor.ex
@@ -2,8 +2,8 @@ defmodule Common.Compressor do
   @moduledoc """
   压缩算法
   """
+  require Logger
 
   def compress(data), do: :zlib.zip(data)
   def decompress(compressed_data), do: :zlib.unzip(compressed_data)
-  
 end
