9590ec1a10ea8beffe6f3cfc53e9a98065341c29
tt67wq <alex@mys4s.cn>
2020-01-17 17:00:26 +0800

    人家不是无趣，而是，懒得对你有趣。

---
+++ apps/common/lib/lzw.ex
@@ -0,0 +1,64 @@
+defmodule Common.Lzw do
+  @moduledoc """
+  Lzw压缩算法
+  """
+  @min_code 256
+
+  require Logger
+
+  def compress(data) do
+    {:ok, compressed_data} =
+      data
+      |> do_compress(%{}, <<>>, @min_code, [])
+      |> Jason.encode()
+
+    Logger.debug("compress_ratio: #{byte_size(compressed_data) / byte_size(data)}")
+    compressed_data
+  end
+
+  def decompress(compressed_data) do
+    {:ok, [h | t]} = Jason.decode(compressed_data)
+    do_decompress(t, @min_code, <<h>>, %{}, <<h>>)
+  end
+
+  defp do_compress("", dict, buffer, _code, acc),
+    do: Enum.reverse([get_dict_idx(buffer, dict) | acc])
+
+  defp do_compress(<<c, rest::binary>>, dict, buffer, code, acc) do
+    item = buffer <> <<c>>
+
+    case get_dict_idx(item, dict) do
+      nil ->
+        # not in dict
+        do_compress(<<rest::binary>>, Map.put(dict, item, code), <<c>>, code + 1, [
+          get_dict_idx(buffer, dict) | acc
+        ])
+
+      _idx ->
+        # ascii or in dict
+        do_compress(<<rest::binary>>, dict, item, code, acc)
+    end
+  end
+
+  defp get_dict_idx(<<c::8>>, _dict) when c < @min_code, do: c
+  defp get_dict_idx(buffer, dict), do: Map.get(dict, buffer)
+
+  defp do_decompress([], _code, _buffer, _dict, acc), do: acc
+
+  defp do_decompress([c | t], code, buffer, dict, acc) do
+    case get_dict_chars(c, dict) do
+      nil ->
+        # not in dict
+        <<first::8, _rest::binary>> = buffer
+        chars = buffer <> <<first>>
+        do_decompress(t, code + 1, chars, Map.put(dict, code, chars), acc <> chars)
+
+      chars ->
+        # in dict
+        do_decompress(t, code + 1, chars, Map.put(dict, code, buffer <> chars), acc <> chars)
+    end
+  end
+
+  defp get_dict_chars(c, _dict) when c < @min_code, do: <<c>>
+  defp get_dict_chars(idx, dict), do: Map.get(dict, idx)
+end
